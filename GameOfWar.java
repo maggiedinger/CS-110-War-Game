32567fc5a4eff4532d5d45d1192002390fe04306
maggiedinger <maggie.dinger@uvm.edu>
2015-04-29 11:33:19 -0400

    create GUI panel

---
+++ GameOfWar.java
@@ -0,0 +1,12 @@
+ 
+import javax.swing.*;
+
+public class GameOfWar {
+    public static void main(String[] args) {
+
+      JFrame frame = new JFrame("Game of War");
+      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      frame.setVisible(true);
+     	frame.pack();
+   }
+}
