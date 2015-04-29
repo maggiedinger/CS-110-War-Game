dd7845cfe07ff9b649a368758779867694724ff0
maggiedinger <maggie.dinger@uvm.edu>
2015-04-29 11:51:37 -0400

    become a border layout

--- GameOfWar.java
+++ GameOfWar.java
@@ -1,18 +1,38 @@
 import java.awt.*;
 import javax.swing.*;
 
-public class GameOfWar {
-    public static void main(String[] args) {
+public class GameOfWar extends JFrame {
+   public GameOfWar(){
 
-      JFrame frame = new JFrame("Game of War");
-      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      //JFrame frame = new JFrame("Game of War");
+      setTitle("Game of War");
+      setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
       
+      setLayout(new BorderLayout());
+         //create four panels
+      JPanel panelButton1 = new JPanel();
+      JPanel panelButton2 = new JPanel();
+      JPanel panel1 = new JPanel();
+      JPanel panel2 = new JPanel();
+         //create buttons
       JButton button1 = new JButton(new ImageIcon("back.jpg"));
       JButton button2 = new JButton(new ImageIcon("back.jpg"));
-      frame.getContentPane().add(button1);
-      frame.getContentPane().add(button2);
-      frame.pack();      
-      frame.setVisible(true);
-
+         //add buttons to panels
+      panelButton1.add(button1);
+      panelButton2.add(button2);
+         //add panels to content pane
+      add(panel1, BorderLayout.CENTER);
+      add(panelButton1, BorderLayout.WEST);
+      add(panel2, BorderLayout.CENTER);
+      add(panelButton2, BorderLayout.EAST);
+      //frame.getContentPane().add(button1);
+      //frame.getContentPane().add(button2);
+      //frame.pack();      
+      //frame.setVisible(true);
+      pack();
+      setVisible(true);
+   }
+   public static void main(String[] args) {
+       new GameOfWar();
    }
 }
