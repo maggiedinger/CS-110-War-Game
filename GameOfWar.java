98da4fbf8e4adf1168a094f6c5df801bf8749e80
maggiedinger <maggie.dinger@uvm.edu>
2015-04-29 11:37:59 -0400

    add buttons with images

--- GameOfWar.java
+++ GameOfWar.java
@@ -1,4 +1,4 @@
- 
+import java.awt.*;
 import javax.swing.*;
 
 public class GameOfWar {
@@ -6,7 +6,13 @@ public class GameOfWar {
 
       JFrame frame = new JFrame("Game of War");
       frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      
+      JButton button1 = new JButton(new ImageIcon("back.jpg"));
+      JButton button2 = new JButton(new ImageIcon("back.jpg"));
+      frame.getContentPane().add(button1);
+      frame.getContentPane().add(button2);
+      frame.pack();      
       frame.setVisible(true);
-     	frame.pack();
+
    }
 }
